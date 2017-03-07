119a240ed59197647dbc6fa5d3ce963a0b3d6ba6
sjp46 <sjp46@njit.edu>
2017-03-07 14:29:11 -0500

    Update add_category.php

---
+++ add_category.php
@@ -0,0 +1,60 @@
+<html>
+
+<!-- the head section -->
+<head>
+    <title>My Guitar Shop</title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Product Manager</h1></header>
+<main>
+    <h1>Category List</h1>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>&nbsp;</th>
+        </tr>
+        
+        <!-- add code for the rest of the table here -->
+    
+    </table>
+
+    <h2>Add Category</h2>
+    <!-- add code for the form here -->
+         <form action="add_product.php" method="post"
+              id="add_product_form">
+
+            <label>Category:</label>
+            <select name="category_id">
+            <?php foreach ($categories as $category) : ?>
+                <option value="<?php echo $category['categoryID']; ?>">
+                    <?php echo $category['categoryName']; ?>
+                </option>
+            <?php endforeach; ?>
+            </select><br>
+
+            <label>Code:</label>
+            <input type="text" name="code"><br>
+
+            <label>Name:</label>
+            <input type="text" name="name"><br>
+
+            <label>List Price:</label>
+            <input type="text" name="price"><br>
+
+            <label>&nbsp;</label>
+            <input type="submit" value="Add Category"><br>
+        </form>
+    
+    <br>
+    <p><a href="index.php">List Products</a></p>
+
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+    </footer>
+</body>
+</html>
