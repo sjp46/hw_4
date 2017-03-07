5bf6e9246b5934abe035413460323880ef8167dd
sjp46 <sjp46@njit.edu>
2017-03-07 15:10:13 -0500

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php 
+    $category_id = filter_input(INPUT_POST, 'category_is', FILTER_VALIDATE_INT);
+    if ($category_id == null ||$category_id == false)
+    {
+            error = "invalid category id.";
+            include ('error.phhp');
+    }
+    else {
+        require_once('database.php');
+        $query = 'delete from categories_guitar1 where categoryID = :category_id';
+        $statement = $db->prepare ($query);
+        $statement->bindValue(":categoryID", $category_id);
+        $statement->execute()
+        $statement->closeCursor();
+        
+        include('category_list.php');
+    }
+?>
