b4c183789726a7b3d74c418003a4c82ea7f0facd
sjp46 <sjp46@njit.edu>
2017-03-07 13:37:22 -0500

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=sql1.njit.edu;dbname=sjp46';
+    $username = 'sjp46';
+    $password = '6IX59hy8y';
 
     try {
         $db = new PDO($dsn, $username, $password);
